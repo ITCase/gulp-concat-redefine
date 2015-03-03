10590d90eff51034f8a228f5454e38635a6c0bad
Arkadiy Zamaraev <arkadiy@bk.ru>
2015-03-03 13:46:23 +0500

    testem add

---
+++ tests/tests.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var hello = require('../hello');
+
+describe('hello', function(){
+    it('says hello world', function(){
+        assert.equal(hello(), 'hello world');
+    });
+    it('says hello to person', function(){
+        assert.equal(hello('Bob'), 'hello Bob');
+    });
+	it('says hello to person', function(){
+        assert.equal(hello('Arkasha'), 'hello Arkasha');
+    });
+});
